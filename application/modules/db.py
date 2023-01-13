8b1619c3c3b8e21320c47e7f137833fd45b0b234
Jan Peterhänsel <jan.peterhaensel@adconova.com>
2023-01-13 17:23:38 +0100

    Changed Database to Mysql for RDS

--- application/modules/db.py
+++ application/modules/db.py
@@ -1,5 +1,6 @@
-import sqlite3 as lite
+import mysql.connector
 import random, string
+from os import environ
 
 class UrlExistsError(Exception):
     pass
@@ -9,37 +10,37 @@ class UrlExistsError(Exception):
 class url_model():
     def __init__(self) -> None:
         query ="CREATE TABLE IF NOT EXISTS urls(short varchar(5),long varchar(255));"
-        self.__lite_query(query)
+        self.__sql_query(query)
         
     def get_short_from_long(self,long: str) -> tuple:
         query = "SELECT short FROM urls WHERE long=?"
-        return self.__lite_query(query,(long,),fetchtype=2)[0]
+        return self.__sql_query(query,(long,),fetchtype=2)[0]
     
     def get_long_from_short(self,short) -> tuple:
         query = "SELECT long FROM urls WHERE short=?"
-        return self.__lite_query(query,(short,),fetchtype=2)[0]
+        return self.__sql_query(query,(short,),fetchtype=2)[0]
     
     def create_short_from_long(self,long) -> str:
-        if self.__lite_query("SELECT True FROM urls WHERE long = ?;",(long,)):
+        if self.__sql_query("SELECT True FROM urls WHERE long = ?;",(long,)):
             raise UrlExistsError("The Value already exists in the database")
         while True:
             short = ''.join(random.choice(string.ascii_letters) for x in range(5))
             print(short)
-            if not self.__lite_query("SELECT True FROM urls WHERE short = ?;",(short,)):
+            if not self.__sql_query("SELECT True FROM urls WHERE short = ?;",(short,)):
                 break
         query = f"INSERT INTO urls(short,long) VALUES(?,?)"
-        self.__lite_query(query,(short,long),fetchtype=3)
+        self.__sql_query(query,(short,long),fetchtype=3)
         return short
     
     def get_all_db_rows(self):
         query = "SELECT short,long FROM urls;"
-        return self.__lite_query(query)
+        return self.__sql_query(query)
     
     def delete_row(self,short):
         query = "DELETE FROM urls WHERE short=?"
-        return self.__lite_query(query,parameters=(short,),fetchtype=3)
+        return self.__sql_query(query,parameters=(short,),fetchtype=3)
     
-    def __lite_query(self,query: str,parameters={},fetchtype: int = 1):
+    def __sql_query(self,query: str,parameters={},fetchtype: int = 1):
         """Handles a database connection to the hardcoded databases.
 
         Args:
@@ -52,7 +53,12 @@ class url_model():
         Returns:
             tuple | bool: Returns the dataset if its a Fetch, otherwise it returns True on a successful Commit
         """
-        conn = lite.connect("db/urls.sqlite")
+        conn = mysql.connector(
+            user = str(environ["DB_USER"]),
+            host = str(environ["DB_HOST"]),
+            database = str(environ["DB_NAME"]),
+            password = str(environ["DB_PASS"])
+        )
         c = conn.cursor()
         data = c.execute(query,parameters)
         if fetchtype == 1:
